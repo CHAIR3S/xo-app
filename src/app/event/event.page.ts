73e8007221ff33765c36eaca71dec2c888f88942
FABRICA <luis.farid@outlook.com>
2024-11-30 12:42:44 -0600

    insert code

--- src/app/event/event.page.ts
+++ src/app/event/event.page.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon } from '@ionic/angular/standalone';
 import { ImageHeaderComponent } from '../image-header/image-header.component';
+import { InsertCodeComponent } from '../insert-code/insert-code.component';
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.page.html',
   styleUrls: ['./event.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonContent, CommonModule, ImageHeaderComponent]
+  imports: [IonIcon, IonContent, CommonModule, ImageHeaderComponent, InsertCodeComponent]
 })
 export class EventPage implements OnInit {
   image: string = '../../assets/img/spoti-utopia.jpg';
--- src/app/insert-code/insert-code.component.ts
+++ src/app/insert-code/insert-code.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-insert-code',
   templateUrl: './insert-code.component.html',
   styleUrls: ['./insert-code.component.scss'],
+  standalone: true
 })
 export class InsertCodeComponent  implements OnInit {
 
